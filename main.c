96996d0664c76659ad606a0ea429e32d0188c801
Aaryan Goel <apg5729@psu.edu>
2020-09-04 01:14:01 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// author: Aaryan Goel apg5729@psu.edu
+// collaborator: Harshal Mahajan hfm5238@psu.edu
+// Siddhartha Kollati szk6147@psu.edu
+// Juma Al-Maskari jpa5637@psu.edu
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int main(void) {
+  char *Celsius = readline("Enter temperature in celsius: ");
+  double Dc = stof(Celsius);
+  double fahrenheit = (Dc* 9/5 + 32 );
+
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", Dc, fahrenheit);
+  return 0;
+}
